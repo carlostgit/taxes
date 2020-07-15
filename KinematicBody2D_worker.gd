71084b68cfb9528a7be756e706a0c1389feb7e9f
carlostgit <carlos.h.corredera@gmail.com>
2020-07-15 23:58:06 +0200

    Se anaden impuestos especiales para el trabajador

---
+++ KinematicBody2D_worker.gd
@@ -0,0 +1,42 @@
+extends "res://KinematicBody2D_slacker.gd"
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+export var _tax_rate_worker = 0.25
+
+export (NodePath) var _government
+
+const MyNode2D_moneyResource = preload("res://Node2D_Coin.tscn")
+
+
+
+func hit_money(var value_arg, var origin_arg, var destiny_arg):
+	var taxes = self._tax_rate_worker*value_arg
+	var money_after_taxes = value_arg-taxes
+	
+	add_money(value_arg)
+	
+	self.call_deferred("pay_taxes",taxes)
+	
+	#var current_money = get_money()
+	if (money_after_taxes>0):
+		var shop_node = self.get_node(_shop_path)
+		self.call_deferred("send_money",money_after_taxes,shop_node)
+
+	
+
+func pay_taxes(var amount_arg):
+	var coin_gov = MyNode2D_moneyResource.instance()
+	self.get_parent().add_child(coin_gov)
+	coin_gov.set_origin_destiny(self,get_node(_government))
+	coin_gov.set_value(amount_arg)
+	add_money(-amount_arg)		
